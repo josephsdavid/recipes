ea08f2a2c0ffa151ee384785a1f0145a138b1957
josephsdavid <josephsd@smu.edu>
2022-02-14 13:00:09 -0600

    initial commit

---
+++ udub.c
@@ -0,0 +1,13 @@
+#include <cstdio>
+union Udoub {
+  double d;
+  unsigned char c[8];
+};
+int main() {
+  Udoub u;
+  u.d = 6.5;
+  for (int i=7;i>=0;i--) printf("%02x",u.c[i]);
+  printf("\n");
+  return 0;
+}
+
